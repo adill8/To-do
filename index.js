cbe04573f3d0531612e1a3d9d5c9e05faaa45d2f
adill8 <adilsheikh061@gmail.com>
2025-05-01 19:17:06 +0500

    Updated task

--- index.js
+++ index.js
@@ -2,35 +2,65 @@ const titleInput = document.getElementById("title");
 const descInput = document.getElementById("desc");
 const taskList = document.getElementById("taskList");
 const btnAdd = document.getElementById("add-btn");
+const btnToggle = document.getElementById("Toggle-btn");
+const body = document.body;
 
 let editMode = false;
 let update = null;
+
+// Add Task
 function addTask() {
-  const title = titleInput.value.trim();
-  const desc = descInput.value.trim();
+  const title = titleInput.value;
+  const desc = descInput.value;
 
   if (!title || !desc) {
-    alert("Title is required");
+    alert("Title and Description is required");
     return;
   }
 
-  //   Task List
+  // Dublicated checked
+
+  let isDublicate = false;
+  const tasks = document.querySelectorAll(".task");
+
+  for(let i = 0; i < tasks.length; i++){
+    const taskTitle = tasks[i].querySelector("h3").innerText;
+    const taskDescription = tasks[i].querySelector("p").innerText;
+
+    if(taskTitle === title && taskDescription === desc){
+      isDublicate = true;
+      break;
+    }
+  }
+
+  if(isDublicate){
+    alert("This task already exits");
+  return;
+  }
+
+  //   Task List with Time
+
+  const timestamp = new Date().toLocaleString();
+
   if (editMode && update) {
     update.querySelector("h3").textContent = title;
   update.querySelector("p").textContent = desc;
+  update.querySelector(".timestamp").textContent = `Created at: ${timestamp}`;
 
     btnAdd.textContent = "Add Task";
     editMode = false;
     update = null;
+    saveData();
   } else {
     const task = document.createElement("div");
     task.className = "task";
     task.innerHTML = `
     <div class="task-content">
-      <input type="checkbox" />
+      <input type="checkbox" class="task-checkbox" />
       <div class="text">
         <h3>${title}</h3>
         <p>${desc}</p>
+        <span class="timestamp">Created at: ${timestamp}</span> 
       </div>
     </div>
     <div class="task-actions">
@@ -44,12 +74,12 @@ function addTask() {
   `;
 
     taskList.appendChild(task);
+    saveData();
   }
   // Clear input fields
   titleInput.value = "";
   descInput.value = "";
 }
-
 
 taskList.addEventListener("click", function (e) {
 
@@ -72,5 +102,57 @@ taskList.addEventListener("click", function (e) {
   if(e.target.closest(".del-btn")){
     const delTask = e.target.closest(".task");
   delTask.remove();
+  saveData();
   }
 });
+
+// Task Completed
+taskList.addEventListener("change", function(e){
+  if(e.target.type === "checkbox"){
+    const task = e.target.closest(".task");
+
+    const isChecked = e.target.checked;
+
+    if(isChecked){
+      task.classList.add("completed")
+    }
+    else{
+      task.classList.remove("completed")
+    }
+
+    const buttons = task.querySelector(".task-actions");
+
+    if(isChecked){
+      buttons.style.display = "none";
+
+    }
+    else{
+      buttons.style.display = "flex";
+    }
+    saveData();
+  }
+});
+
+// Local Storage Data
+function saveData(){
+    localStorage.setItem("data",  taskList.innerHTML);
+}
+
+// Show Data
+function showTask(){
+  taskList.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+// Dark Mode Section
+btnToggle.addEventListener("click", function(){
+  body.classList.toggle("dark-mode");
+
+  if(body.classList.contains("dark-mode")){
+    btnToggle.textContent = "Light Mode";
+  }
+  else{
+    btnToggle.textContent = "Dark Mode";
+  }
+})
+
